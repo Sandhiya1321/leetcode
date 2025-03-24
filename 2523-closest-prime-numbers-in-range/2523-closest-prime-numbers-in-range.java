acdbd46c9f61e27ba3c7915986707663962f54a0
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-03-24 19:22:47 +0530

    Time: 1 ms (97.03%), Space: 41.1 MB (93.77%) - LeetHub

--- 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
+++ 2523-closest-prime-numbers-in-range/2523-closest-prime-numbers-in-range.java
@@ -1,44 +1,39 @@
 class Solution {
     public int[] closestPrimes(int left, int right) {
-       int ans[]=new int[2];
-       ans[0]=-1;
-       ans[1]=-1;
-       int r=0;
-       for(int i=left;i<=right;i++){
-        if(ans[1]-ans[0]==2|| ans[1]-ans[0]==1)
-        break;
-        if(primeval(i)){
-            if(ans[0]==-1){
-                ans[0]=i;
-            }
-            else if(ans[1]==-1){
-                ans[1]=i;
-                r=i;
-            }
-            else{
-                if(ans[1]-ans[0]<i-r){
+        int ans[] = new int[2];
+        ans[0]=-1;
+        ans[1]=-1;
+        int r=0;
+        for(int i=left;i<=right;i++){
+            if(ans[1]-ans[0]==2 || ans[1]-ans[0]==1) break;
+            if(isprime(i)){
+                if(ans[0]==-1) ans[0]=i;
+                else if(ans[1]==-1) {
+                    ans[1]=i;
                     r=i;
                 }
-            }
-        }
-       }
-       if(ans[0]==-1||ans[1]==-1){
-        return new int[]{-1,-1};
-       }
-       return ans;
+                else{
+                    if(ans[1]-ans[0]<i-r) r = i;
+                    else{
+                    ans[0]=r;
+                    ans[1] = i;
+                    r=i;
+                    }
+                }
+     }
     }
-    public static boolean primeval(int val){
-        if(val==1){
-            return true;
-        }
-       if(val==1||val%2==0){
-        return false;
-       }
-        for(int i=2;i<val;i++){
-            if(val%i==0){
-               return false;
+    if(ans[0]==-1 || ans[1]==-1) return new int[]{-1,-1};
+     return ans;
+}
+
+    public boolean isprime(int n){
+         if(n==2) return true;
+         if(n==1 || n%2==0) return false;
+         for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false; 
             }
         }
-        return true;
+        return true; 
     }
 }
