41dd0caa8c2457b30db9a0548ddac93998ea35c7
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-05-01 19:55:51 +0530

    Time: 0 ms (100%), Space: 44.5 MB (78.3%) - LeetHub

---
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int[] num:image){
+            for(int i=0;i<(image[0].length+1)/2;i++){
+                int temp=num[i]^1;
+                num[i]=num[image[0].length-i-1]^1;
+                num[image[0].length-i-1]=temp;
+            }
+        }
+        return image;
+    }
+}
