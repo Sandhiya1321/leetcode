af7d64fa745975f7643d66f0b766cda839e9d69c
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-05-18 16:16:41 +0530

    Time: 1 ms (100%), Space: 41.8 MB (54.24%) - LeetHub

--- 0390-elimination-game/0390-elimination-game.java
+++ 0390-elimination-game/0390-elimination-game.java
@@ -1,15 +1,8 @@
 class Solution {
     public int lastRemaining(int n) {
-        return moving(n,true);
-    }
-    public int moving(int n,boolean leftToRight){
         if(n==1){
             return 1;
         }
-        if(leftToRight){
-            return 2*moving(n/2,false);
-        }else{
-            return 2*moving(n/2,true)-(n%2==0?1:0);
-        }
+        return 2*(n/2-lastRemaining(n/2)+1);
     }
 }
