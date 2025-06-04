96a39a8ee4a28c90f1ccfc35b260f0f106aa0085
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-06-04 20:22:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int n=9;
+        boolean[][] row=new boolean[n][n];
+        boolean[][] col=new boolean[n][n];
+        boolean[][] boards=new boolean[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='.'){
+                    continue;
+                }
+                int num=board[i][j]-'1';
+                int box=(i/3)*3+(j/3);
+                if(row[i][num]||col[j][num]){
+                    return false;
+                }
+                row[i][num]=true;
+                col[j][num]=true;
+                boards[box][num]=true;
+            }
+        }
+        return true;
+    }        
+}
