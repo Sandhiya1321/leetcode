8218f3449957c3f0cadc67231ae16c5dcfa5b0b9
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-06-26 22:41:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String convertDateToBinary(String date) {
+       String[] part=date.split("-");
+       int year=Integer.ParseInt(part[0]); 
+        int year=Integer.ParseInt(part[0]); 
+    }
+}
