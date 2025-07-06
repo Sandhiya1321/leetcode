2154a584a8508287bcacdf5eef7c78c6aeff98c7
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-07-06 18:16:59 +0530

    Time: 1 ms (99.8%), Space: 50.6 MB (27.69%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,40 +1,42 @@
+
+
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
-        int count1=0;
-        int count2=0;
-        int element1=Integer.MIN_VALUE;
-        int element2=Integer.MIN_VALUE;
-        int n=nums.length;
-        for(int i=0;i<n;i++){
-            if(count1==0&&nums[i]!=element2){
-                count1=1;
-                element1=nums[i];
-            }else if(count2==0&&nums[i]!=element1){
-                count2=1;
-                element2=nums[i];
-            }
-            else if(nums[i]==element1){
+        int n = nums.length;
+        List<Integer> result = new ArrayList<>();
+
+        // Step 1: Find two potential candidates
+        int count1 = 0, count2 = 0;
+        int element1 = 0, element2 = 0;
+
+        for (int num : nums) {
+            if (num == element1) {
                 count1++;
-            }else if(nums[i]==element2){
+            } else if (num == element2) {
                 count2++;
+            } else if (count1 == 0) {
+                element1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                element2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
             }
         }
-        int c1=0;
-        int c2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==element1){
-                c1++;
-            }else if(nums[i]==element2){
-                c2++;
-            }
+
+        // Step 2: Verify actual frequencies
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == element1) count1++;
+            else if (num == element2) count2++;
         }
-        if(c1>n/3){
-            list.add(element1);
-        }
-        if(c2>n/3){
-            list.add(element2);
-        }
-        return list;
+
+        if (count1 > n / 3) result.add(element1);
+        if (count2 > n / 3) result.add(element2);
+
+        return result;
     }
 }
