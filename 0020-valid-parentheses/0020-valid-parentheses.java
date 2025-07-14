e7f69838fa253f9ef8c2d7a03cf0100ea86c2ee7
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-07-14 21:26:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==1){
+            return false;
+        }
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='('||ch=='['||ch=='{'){
+                stack.push(ch);
+            }else{
+                if(stack.isEmpty())
+                return false;
+                char first=stack.pop();
+                if((ch==')'&&first!='(')||(ch==']'&&first!='[')||(ch=='}'&&first!='{')){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
