8c4b958e8a8e5fac100163dfcce0bcee6daae581
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-08-19 19:31:06 +0530

    Time: 1 ms (98.41%), Space: 41.1 MB (55.39%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,5 +1,16 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        return stones[stones.length-1];
+        PriorityQueue<Integer> max=new PriorityQueue<>(Collections.reverseOrder());
+        for(int stone:stones){
+            max.add(stone);
+        }
+        while(max.size()>1){
+            int stone1=max.poll();
+            int stone2=max.poll();
+            if(stone1!=stone2){
+                max.add(stone1-stone2);
+            }
+        }
+        return max.isEmpty()?0:max.poll();       
     }
 }
