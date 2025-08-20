9e66175e22b256944b3cda22df0637de33e8363c
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-08-20 10:22:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int x0=coordinates[0][0];
+        int x0=coordinates[0][0];
+        int x0=coordinates[0][0];
+    }
+}
