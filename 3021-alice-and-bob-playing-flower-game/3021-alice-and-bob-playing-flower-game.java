515c56dd931c7ecd2d349da211a1e01d8688a8a9
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-08-29 18:25:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        long n1=(n+1)/2;
+        long n2=n/2;
+        long m1=(m+1)/2;
+        long m2=m/2;
+
+        return n1*n2+m1*m2;
+    }
+}
