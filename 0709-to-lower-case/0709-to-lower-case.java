bae45b618f5a687e9f9143b169efa2aad23f3b1e
Sandhiya S <sandhiyasaprkle1234@gmail.com>
2025-11-20 15:22:40 +0530

    Time: 0 ms (100%), Space: 43 MB (8.98%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,9 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-         String str=" ";
-        for(int i=0;i<s.length();i++){
-           str=s.toLowerCase();
-        }
-        return str;
+        
+        return s.toLowerCase();
     }
 }
